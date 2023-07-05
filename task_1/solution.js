b2d8758168cc3c7653919bb5f658512ce1d6ad46
Olesya Sbytova <Sbitovalesya@mail.ru>
2023-07-05 19:19:46 +0700

    task2 done_task1 changed

--- task_1/solution.js
+++ task_1/solution.js
@@ -42,7 +42,7 @@ function addInCartSumChange(sum, delta) {
     // Задание №1.3. Увеличение суммы в корзине
 
     let newSum = oldSum + difference;
-    newSumText = newSum + ' ₽';
+    let newSumText = newSum + ' ₽';
     newSumText = (`${newSum} ₽`);
     
     // Конец решения задания №1.3.
